b5a041210b833c2d22f5e7d621e8a6b976707456
Oro31 <60878633+Oro31@users.noreply.github.com>
2021-04-09 12:57:16 +0200

    adding rep c_files with proper Makefile

---
+++ c_files/draw.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvalton <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/31 07:46:41 by rvalton           #+#    #+#             */
+/*   Updated: 2021/04/06 08:42:09 by rvalton          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	ft_draw(t_all *vars)
+{
+	/*vars->img.img = mlx_new_image(vars->mlx, vars->rsl.w, vars->rsl.h);
+	vars->img.addr = mlx_get_data_addr(vars->img.img, &vars->img.bpp, 
+			&vars->img.ll, &vars->img.e);
+	ft_mymlx_pixelput(vars, &vars->img);*/
+	mlx_put_image_to_window(vars->mlx, vars->win, vars->spr.data[3].img, 0, 0);
+}
---
+++ c_files/image.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   image.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvalton <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/08 10:50:28 by rvalton           #+#    #+#             */
+/*   Updated: 2021/04/08 11:03:50 by rvalton          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	ft_mymlx_pixelput(t_all *vars, t_data *data)
+{
+	char	*dst;
+	int	x;
+	int	y;
+	int	px;
+	int	py;
+
+	x = 0;
+	while (x < vars->rsl.w)
+	{
+		y = 0;
+		while (y < vars->rsl.h)
+		{
+			dst = data->addr + (y * data->ll + x * 
+					(data->bpp / 8));
+			px = x;
+			py = y;
+			*(unsigned int*)dst = 
+				ft_get_xpm_pixel(&vars->spr.data[3], 
+						px, py);
+			y++;
+		}
+		x++;
+	}
+}
+
+int	ft_get_xpm_pixel(t_data *data, int y, int x)
+{
+	int	color;
+
+	color = (int)data->addr + (y * data->ll + x * (data->bpp / 8));
+	return (color);
+}
